af6b40fdc3765c22195d3019fb664df0bea859ae
jayasurya072001 <jayasurya.s@mediafirewall.ai>
2025-07-24 17:34:13 +0530

    updated environment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/vision", // Replace with your API base URL
+  baseURL: API_BASE_URL, // Replace with your API base URL
 });
 
 export default api;
